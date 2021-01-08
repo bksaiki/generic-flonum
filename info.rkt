3c721fe27983514fdb7e712e4e5725004ac51d57
Brett Saiki <bksaiki@gmail.com>
2021-01-07 22:17:16 -0800

    Fix documentation

--- info.rkt
+++ info.rkt
@@ -1,9 +1,9 @@
 #lang info
 
-(define collection "generic-flonum")
+(define name "generic-flonum")
 (define deps '("math-lib" "base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/gfl.scrbl" ())))
+(define scribblings '(("scribblings/generic-flonum.scrbl")))
 (define pkg-desc "Generic IEEE-754 floating-point numbers")
 (define version "1.0")
 (define pkg-authors '("Brett Saiki"))
