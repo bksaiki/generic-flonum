8e2dd85a49dd396cee12fc72d40a49d4b331b829
Brett Saiki <bksaiki@gmail.com>
2021-01-19 21:01:55 -0800

    Fix dependencies, remove github stuff

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 
 (define name "generic-flonum")
 (define deps '("math-lib" "base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("scribble-lib" "rackunit-lib" "racket-doc" "math-doc"))
 (define scribblings '(("scribblings/generic-flonum.scrbl")))
 (define pkg-desc "Generic IEEE-754 floating-point numbers")
 (define version "1.0")
